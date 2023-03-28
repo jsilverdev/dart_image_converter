c75f6c483d84c354f8d22454484fde1f7132bb7e
Julio Rios <jrp8900@gmail.com>
2023-03-28 18:10:03 -0500

    Refactor utils/image_utils test

--- test/utils/image_utils_test.dart
+++ test/utils/image_utils_test.dart
@@ -7,50 +7,108 @@ import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
 void main() {
+
   test(
-    'Should capture a file and save as JPG',
+    'Should convert a jpeg file then resize and save as jpg',
     () async {
-      //arrange
-      final width = _randomInt(100, 999);
-      final height = width;
-      Map<String, File> mapInputFiles = {
-        for (var file in Directory(path.absolute("test/_data/input"))
-            .listSync()
-            .whereType<File>()
-            .toList())
-          _newOutPath(file.path, "jpg"): file
-      };
-
-      //act
-      List<img.Image?> outputImages = [];
-      for (var entry in mapInputFiles.entries) {
-        resizeAndSaveFileAsJpg(
-          entry.value,
-          width: width,
-          height: height,
-          toSavePath: entry.key,
-        );
-        outputImages.add(
-          await img.decodeImageFile(entry.key),
-        );
-      }
-
-      //assert
-      for (var image in outputImages) {
-        expect(image, isNotNull);
-        expect(image?.isValid, equals(true));
-        expect(image?.width, equals(width));
-        expect(image?.height, equals(height));
-        expect(image?.hasAlpha, equals(false));
-      }
+      _testResizeAndSaveFileAsJpg("jpeg");
     },
   );
+
+  test(
+    'Should convert a jpg file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("jpg");
+    },
+  );
+
+  test(
+    'Should convert a png file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("png");
+    },
+  );
+
+  test(
+    'Should convert a gif file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("gif");
+    },
+  );
+
+  test(
+    'Should convert a bmp file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("bmp");
+    },
+  );
+
+  test(
+    'Should convert a ico file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("ico");
+    },
+  );
+
+  test(
+    'Should convert a tiff file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("tiff");
+    },
+  );
+
+  test(
+    'Should convert a webp file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("webp");
+    },
+  );
+
+  test(
+    'Should convert a psd file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("psd");
+    },
+  );
+
+  test(
+    'Should convert a pdf file then resize and save as jpg',
+    () async {
+      _testResizeAndSaveFileAsJpg("pdf");
+    },
+  );
+
+}
+
+Future<void> _testResizeAndSaveFileAsJpg(String extension) async {
+  //arrange
+  final width = _randomInt(100, 999);
+  final height = width;
+
+  final file = File(path.absolute("test/_data/input/sample.$extension"));
+  final toSavePath = _newOutJpgPath(extension);
+
+  //act
+  resizeAndSaveFileAsJpg(
+    file,
+    width: width,
+    height: height,
+    toSavePath: toSavePath,
+  );
+  img.Image? image = await img.decodeImageFile(toSavePath);
+
+  //assert
+  expect(image, isNotNull);
+  expect(image?.isValid, equals(true));
+  expect(image?.width, equals(width));
+  expect(image?.height, equals(height));
+  expect(image?.hasAlpha, equals(false));
 }
 
 int _randomInt(int min, int max) {
   return min + Random().nextInt(max - min);
 }
 
-String _newOutPath(String filePath, String extension) {
-  return "${path.absolute("test/_data/output/")}${path.extension(filePath).substring(1)}.$extension";
+String _newOutJpgPath(String originExtension) {
+  return "${path.absolute("test/_data/output/")}$originExtension.jpg";
 }
